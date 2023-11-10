e2b26bf79e528a30e2f25104b2266c323592dc63
Marcus <marcus@br-j.dk>
2023-11-10 11:22:40 +0100

    123

--- src/client-ctrl.py
+++ src/client-ctrl.py
@@ -1,3 +1,52 @@
+import tkinter as tk
+
+class JoystickApp:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Min Fede Joystick App")
+
+        self.canvas = tk.Canvas(self.master, width=400, height=400, bg="pink")
+        self.canvas.pack()
+
+        self.joystick = self.canvas.create_oval(190, 190, 210, 210, fill="red")
+
+        self.canvas.create_text(100, 300, text="Venstre, Ned", font=("Helvetica", 12))
+        self.canvas.create_text(100, 100, text="Venstre, Op", font=("Helvetica", 12))
+        self.canvas.create_text(300, 300, text="Højre, Ned", font=("Helvetica", 12))
+        self.canvas.create_text(300, 100, text="Højre, Op", font=("Helvetica", 12))
+        self.canvas.create_text(200, 50, text="Op", font=("Helvetica", 12))
+        self.canvas.create_text(350, 200, text="Højre", font=("Helvetica", 12))
+        self.canvas.create_text(200, 350, text="Ned", font=("Helvetica", 12))
+        self.canvas.create_text(50, 200, text="Venstre", font=("Helvetica", 12))
+
+        self.canvas.bind("<B1-Motion>", self.move_joystick)
+        self.canvas.bind("<ButtonRelease-1>", self.tilbage_til_start)
+
+        self.update_coordinates()
+
+    def move_joystick(self, event):
+        x = event.x
+        y = event.y
+
+        x = max(10, min(390, x))
+        y = max(10, min(390, y))
+
+        self.canvas.coords(self.joystick, x - 10, y - 10, x + 10, y + 10)
+
+    def tilbage_til_start(self, event):
+        self.canvas.coords(self.joystick, 190, 190, 210, 210)
+
+    def update_coordinates(self):
+        print(self.canvas.coords(self.joystick))
+        self.master.after(100, self.update_coordinates)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = JoystickApp(root)
+    root.mainloop()
+
+
+
 import seocket
 import keyboard
 
@@ -25,3 +74,4 @@ while True:
         send_styring('d')
     elif keyboard.is_pressed('q'):
         send_styring('q')
+
